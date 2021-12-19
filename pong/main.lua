826caad5c3e352db4e086be4800064195b99889f
MikeNotu <mikebateman98@gmail.com>
2021-12-19 13:06:52 -0600

    Version 3

--- pong/main.lua
+++ pong/main.lua
@@ -28,16 +28,20 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
+PADDLE_SPEED = 200
+
 
 --[[
     Runs when the game first starts up, only once; used to initialize the game.
 ]]
 function love.load()
 
-
     love.graphics.setDefaultFilter('nearest','nearest')
+    
 
     smallFont = love.graphics.newFont('font.ttf',8)
+
+    scoreFont = love.graphics.newFont('font.ttf',32)
 
     love.graphics.setFont(smallFont)
 
@@ -46,7 +50,35 @@ function love.load()
         resizable = false,
         vsync = true
     })
+
+
+    player1Score = 0
+    player2Score = 0
+
+    player1Y = 30
+    player2Y = VIRTUAL_HEIGHT - 50
+
 end
+
+function love.update(dt)
+
+    if love.keyboard.isDown('w') then
+        player1Y = player1Y + -PADDLE_SPEED * dt
+
+    elseif love.keyboard.isDown('s') then
+            player1Y = player1Y + PADDLE_SPEED * dt
+    end
+    
+    if love.keyboard.isDown('up') then
+
+        player2Y = player2Y + -PADDLE_SPEED * dt
+
+    elseif love.keyboard.isDown('down')then
+            player2Y = player2Y + PADDLE_SPEED * dt
+    end
+end
+
+
 
 function love.keypressed(key)
 
@@ -66,11 +98,16 @@ function love.draw()
 
     love.graphics.clear(40,45,52,255)
 
+    love.graphics.setFont(smallFont)
     love.graphics.printf('Hello Pong!',0,20,VIRTUAL_WIDTH,'center')
 
-    love.graphics.rectangle('fill',10,30,5,20)
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH/2-50,VIRTUAL_HEIGHT/3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH/2+30,VIRTUAL_HEIGHT/3)
 
-    love.graphics.rectangle('fill',VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+    love.graphics.rectangle('fill',10,player1Y,5,20)
+
+    love.graphics.rectangle('fill',VIRTUAL_WIDTH - 10, player2Y, 5, 20)
 
     love.graphics.rectangle('fill',VIRTUAL_WIDTH/2-2,VIRTUAL_HEIGHT/2-2,4,4)
 
