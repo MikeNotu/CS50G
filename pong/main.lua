f12eecbb14bb81fc3a50d70c05bb257a630d2844
MikeNotu <mikebateman98@gmail.com>
2021-12-19 14:38:29 -0600

    Added FPS Display

--- pong/main.lua
+++ pong/main.lua
@@ -137,5 +137,13 @@ function love.draw()
 
     ball:render()
     
+    displayFPS()
+    
     push:apply('end')
+end
+
+function displayFPS()
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(0,255,0,255)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()),10,10)
 end
