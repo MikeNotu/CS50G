e4de30886d14f57c8698a88adbc0ca06e5b931ae
MikeNotu <mikebateman98@gmail.com>
2021-12-19 14:28:40 -0600

    Added Obejected Orieneted Programming Structure

--- pong/main.lua
+++ pong/main.lua
@@ -22,6 +22,12 @@
 
 push = require 'push'
 
+Class = require 'class'
+
+require 'Paddle'
+
+require 'Ball'
+
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
@@ -51,18 +57,10 @@ function love.load()
         vsync = true
     })
 
+    player1 = Paddle(10,30,5,20)
+    player2 = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 30,5,20)
 
-    -- player1Score = 0
-    -- player2Score = 0
-
-    player1Y = 30
-    player2Y = VIRTUAL_HEIGHT - 50
-
-    ballX = VIRTUAL_WIDTH / 2 - 2
-    ballY = VIRTUAL_HEIGHT / 2 - 2
-
-    ballDX = math.random(2) == 1 and 100 or -100
-    ballDY = math.random(-50,50)
+    ball = Ball(VIRTUAL_WIDTH/ 2 - 2,VIRTUAL_HEIGHT/2 -2, 4,4)
 
     gameState = 'start'
 end
@@ -70,24 +68,29 @@ end
 function love.update(dt)
 
     if love.keyboard.isDown('w') then
-        player1Y = math.max(0,player1Y + -PADDLE_SPEED * dt)
+        player1.dy = -PADDLE_SPEED
 
     elseif love.keyboard.isDown('s') then
-            player1Y = math.min(VIRTUAL_HEIGHT - 20, player1Y + PADDLE_SPEED * dt)
+        player1.dy = PADDLE_SPEED
+    else
+        player1.dy = 0
     end
     
     if love.keyboard.isDown('up') then
+        player2.dy = -PADDLE_SPEED
 
-        player2Y = math.max(0,player2Y + -PADDLE_SPEED * dt)
-
-    elseif love.keyboard.isDown('down')then
-            player2Y = math.min(VIRTUAL_HEIGHT - 20, player2Y + PADDLE_SPEED * dt)
+    elseif love.keyboard.isDown('down') then
+        player2.dy = PADDLE_SPEED
+    else
+        player2.dy = 0
     end
 
     if gameState == 'play' then
-        ballX = ballX + ballDX * dt
-        ballY = ballY + ballDY * dt
+        ball:update(dt)
     end
+
+    player1:update(dt)
+    player2:update(dt)
 end
 
 
@@ -102,11 +105,7 @@ function love.keypressed(key)
         else
             gameState = 'start'
 
-            ballX = VIRTUAL_WIDTH / 2 - 2
-            ballY = VIRTUAL_HEIGHT / 2 - 2
-
-            ballDX = math.random(2) == 1 and 100 or -100
-            ballDY = math.random(-50,50) * 1.5
+            ball:reset()
         end 
     end     
 end
@@ -133,11 +132,10 @@ function love.draw()
     -- love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH/2-50,VIRTUAL_HEIGHT/3)
     -- love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH/2+30,VIRTUAL_HEIGHT/3)
 
-    love.graphics.rectangle('fill',10,player1Y,5,20)
+    player1:render()
+    player2:render()
 
-    love.graphics.rectangle('fill',VIRTUAL_WIDTH - 10, player2Y, 5, 20)
-
-    love.graphics.rectangle('fill',ballX,ballY,4,4)
-
+    ball:render()
+    
     push:apply('end')
 end
