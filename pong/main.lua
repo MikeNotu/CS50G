5c7cf2d8712a82ccf0790fb0c1b0ab8996ea892f
MikeNotu <mikebateman98@gmail.com>
2021-12-19 10:46:04 -0600

    Restored old push.lua file and movedd to same LOVE version as the course

--- pong/main.lua
+++ pong/main.lua
@@ -37,6 +37,10 @@ function love.load()
 
     love.graphics.setDefaultFilter('nearest','nearest')
 
+    smallFont = love.graphics.newFont('font.ttf',8)
+
+    love.graphics.setFont(smallFont)
+
     push:setupScreen(VIRTUAL_WIDTH,VIRTUAL_HEIGHT,WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = false,
@@ -60,12 +64,15 @@ function love.draw()
 
     push:apply('start')
 
-    love.graphics.printf(
-        'Hello Pong!',          -- text to render
-        0,                      -- starting X (0 since we're going to center it based on width)
-        VIRTUAL_HEIGHT / 2 - 6,  -- starting Y (halfway down the screen)
-        VIRTUAL_WIDTH,           -- number of pixels to center within (the entire screen here)
-        'center')               -- alignment mode, can be 'center', 'left', or 'right'
+    love.graphics.clear(40,45,52,255)
+
+    love.graphics.printf('Hello Pong!',0,20,VIRTUAL_WIDTH,'center')
+
+    love.graphics.rectangle('fill',10,30,5,20)
+
+    love.graphics.rectangle('fill',VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT - 50, 5, 20)
+
+    love.graphics.rectangle('fill',VIRTUAL_WIDTH/2-2,VIRTUAL_HEIGHT/2-2,4,4)
 
     push:apply('end')
 end
